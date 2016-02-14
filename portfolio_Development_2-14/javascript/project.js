a6414bb5e8c1c2cbd317630ee76190854964fa70
jdmellon <jeremydmellon@gmail.com>
2016-02-14 17:21:04 -0500

    test

--- portfolio_Development_2-14/javascript/project.js
+++ portfolio_Development_2-14/javascript/project.js
@@ -44,3 +44,34 @@ $('#return-to-top').click(function() {      // When arrow is clicked
 
 // ----------------------------------------------------------
 
+
+
+// Responsive Navigation
+
+$(document).on("ready",function() {
+  // 0 = hide, 1 = visible
+  var menuState = 0;
+  //if($(".mini-menu-options").is(":hidden")) {
+    $(".btn-select").on("click",function() {
+      if(menuState === 0) {
+        $(".mini-menu-options").slideDown("slow");
+        menuState = 1;
+      } else {
+        $(".mini-menu-options").slideUp("slow");
+        menuState = 0;
+      }
+    });
+  //}
+  $(".mini-menu-options li").on("click", function() {
+    var numHijos = $(this).children().length;
+    if(numHijos < 2) {
+      // hide the menu
+      $(".mini-menu-options").hide("fast");
+
+      var texto = $(this).text();
+
+      $(".menu-select .menu-actual").text(texto);
+    }
+    menuState = 0;
+  });
+});
